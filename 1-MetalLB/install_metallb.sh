f9686287aee5dab5a06e7ad3249cff55fac252a8
karnivor-blue <60560033+karnivor-blue@users.noreply.github.com>
2020-10-02 22:10:03 -0400

    Update install_metallb.sh

--- 1-MetalLB/install_metallb.sh
+++ 1-MetalLB/install_metallb.sh
@@ -1,3 +1,4 @@
+# sourced from https://metallb.universe.tf/installation/
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 # On first install only
